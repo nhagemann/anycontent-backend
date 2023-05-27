6a68187a6f3ee6457822f7da10821425783f6b7c
Nils Hagemann <mail@nilshagemann.de>
2023-05-27 16:24:10 +0200

    have a basic authentication

--- _bundle/src/Modules/Start/Controller/StartController.php
+++ _bundle/src/Modules/Start/Controller/StartController.php
@@ -8,8 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-
+#[IsGranted('ROLE_ANYCONTENT')]
 class StartController extends AbstractController
 {
     public function __construct(private RepositoryManager $repositoryManager){}
