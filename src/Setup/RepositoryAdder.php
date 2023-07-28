c2ab4190552fc3ed3e0478efbc782d338e91d9de
Nils Hagemann <mail@nilshagemann.de>
2023-07-28 09:57:52 +0200

    allow more connection types

--- src/Setup/RepositoryAdder.php
+++ src/Setup/RepositoryAdder.php
@@ -9,7 +9,6 @@ use AnyContent\Connection\Configuration\ContentArchiveConfiguration;
 use AnyContent\Connection\Configuration\MySQLSchemalessConfiguration;
 use AnyContent\Connection\Configuration\RecordsFileConfiguration;
 use AnyContent\Connection\FileManager\DirectoryBasedFilesAccess;
-use Symfony\Component\Finder\Finder;
 
 /**
  * Adds all configured repositories
@@ -26,6 +25,8 @@ class RepositoryAdder
     {
         $this->repositoryManager = $repositoryManager;
 
+        $recordFiles = [];
+
         foreach ($this->connections as $connection) {
             $name = $connection['name'];
             $type = $connection['type'];
@@ -34,16 +35,17 @@ class RepositoryAdder
 
             switch ($type) {
                 case 'recordsfile':
-                    $data = $connection['data'];
-                    $cmdl = $connection['cmdl'];
-                    $repository = $this->addRecordsFileConnection($name, $data, $cmdl);
+                    $dataType = isset($connection['content_file']) ? 'content' : 'config';
+                    $dataFile = $connection['content_file'] ?? $connection['config_file'];
+                    $filesPath = $connection['files_path'] ?? null;
+                    $recordFiles[$name][] = ['cmdl_file' => $connection['cmdl_file'], 'data_type' => $dataType, 'data_file' => $dataFile, 'files_path' => $filesPath];
                     break;
                 case 'recordfiles':
                     //$path = $connection['path'];
                     //$repository = $this->addRecordFilesConnection($name, $path);
                     break;
                 case 'contentarchive':
-                    $path = $connection['path'];
+                    $path = $connection['data_path'];
                     $repository = $this->addContentArchiveConnection($name, $path);
                     break;
                 case 'mysql':
@@ -52,7 +54,7 @@ class RepositoryAdder
                     $user = $connection['db_user'];
                     $password = $connection['db_password'];
                     $port = $connection['db_port'];
-                    $path = $connection['path'];
+                    $path = $connection['cmdl_path'];
                     $repository = $this->addMySQLConnection($name, $host, $database, $user, $password, $port, $path);
                     break;
             }
@@ -61,49 +63,35 @@ class RepositoryAdder
                 continue;
             }
 
-            if (isset($connection['files'])) {
-                $files = $connection['files'];
-                $this->addFileManager($repository, $files);
+            if (isset($connection['files_path'])) {
+                $this->addFileManager($repository, $connection['files_path']);
             }
+
+            $this->addRecordsFileConnection($recordFiles);
         }
     }
 
-    private function addRecordsFileConnection(string $name, string $dataFolder, string $cmdlFolder): Repository
+    private function addRecordsFileConnection($recordFiles): void
     {
-        if (!file_exists($cmdlFolder)) {
-            throw new AnyContentBackendException(sprintf('Cannot find cmdl folder %s for connection %s', $cmdlFolder, $name));
-        }
-        if (!file_exists($dataFolder)) {
-            throw new AnyContentBackendException(sprintf('Cannot find data folder %s for connection %s', $dataFolder, $name));
-        }
-
-        // Add all content types
-        $finder = new Finder();
-        $configuration = new RecordsFileConfiguration();
-        foreach ($finder->in($cmdlFolder)->depth(0)->name('*.cmdl')->files() as $file) {
-            $contentTypeName = $file->getFilenameWithoutExtension();
-            $dataFileName = sprintf('%s/%s.json', $file->getPath(), $file->getFilenameWithoutExtension());
-            $cmdlFilename = (string)$file->getRealPath();
-            $configuration->addContentType($contentTypeName, $cmdlFilename, $dataFileName);
-        }
-
-        // Add config types, if there is a config subfolder
-        $finder = new Finder();
-        $cmdlFolder = sprintf('%s/config', $cmdlFolder);
-        if (file_exists($cmdlFolder)) {
-            foreach ($finder->in($cmdlFolder)->depth(0)->name('*.cmdl')->files() as $file) {
-                $configTypeName = $file->getFilenameWithoutExtension();
-                $dataFileName = sprintf('%s/%s.json', $file->getPath(), $file->getFilenameWithoutExtension());
-                $cmdlFilename = (string)$file->getRealPath();
-                $configuration->addConfigType($configTypeName, $cmdlFilename, $dataFileName);
+        foreach ($recordFiles as $name => $dataTypes) {
+            $filesPath = null;
+            $configuration = new RecordsFileConfiguration();
+            foreach ($dataTypes as $dataType) {
+                if ($dataType['data_type'] === 'content') {
+                    $configuration->addContentType(basename($dataType['cmdl_file'], '.cmdl'), $dataType['cmdl_file'], $dataType['data_file']);
+                }
+                if ($dataType['data_type'] === 'config') {
+                    $configuration->addConfigType(basename($dataType['cmdl_file'], '.cmdl'), $dataType['cmdl_file'], $dataType['data_file']);
+                }
+                $filesPath = $dataType['files_path'] ?? $filesPath;
+            }
+            $connection = $configuration->createReadWriteConnection();
+            $repository = new Repository($name, $connection);
+            $this->repositoryManager->addRepository($name, $repository);
+            if ($filesPath !== null) {
+                $this->addFileManager($repository, $filesPath);
             }
         }
-
-        $connection = $configuration->createReadWriteConnection();
-
-        $repository = new Repository($name, $connection);
-        $this->repositoryManager->addRepository($name, $repository);
-        return $repository;
     }
 
     private function addContentArchiveConnection(string $name, string $path): Repository
